de9731d6c798e0aa7fc2c5ad54425bd24bd1d808
Romario Teixeira <drt.romario@hotmail.com>
2021-05-24 00:08:34 +0100

    all baswe requirements done, need navigations

--- mobileapp/lib/ui/common.dart
+++ mobileapp/lib/ui/common.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-Color mTextColor = Color.fromARGB(255, 15, 32, 39);
-Color mtextCOlor2 = Color.fromARGB(255, 241, 243, 246);
-Color mBackground = Color.fromARGB(255, 12, 21, 12);
+Color mTextColor = Color.fromARGB(255, 34, 43, 76);
+Color mTextColor2 = Colors.grey;
+
+Color mPrimaryColor = Color.fromARGB(255, 55, 126, 255);
+Color mSecondaryColor = Color.fromARGB(255, 241, 243, 246);
 
 const SIZE_XSMALL = 12.0;
 const SIZE_SMALL = 15.0;
 const SIZE_MEDIUM = 18.0;
 const SIZE_LARGE = 20.0;
-const SIZE_XLARGE = 35.0;
+const SIZE_XLARGE = 30.0;
 const SIZE_XXLARGE = 45.0;
 
 TextStyle smallTextStyle(Color color, [FontWeight fw = FontWeight.normal]) {
@@ -60,27 +61,12 @@ Widget simpleAppBar(BuildContext context) {
   );
 }
 
-Widget customTextField(TextEditingController controller, Function action) {
-  return Container(
-    margin: EdgeInsets.symmetric(horizontal: 30),
-    child: TextField(
-      controller: controller,
-      maxLines: 1,
-      inputFormatters: [
-        FilteringTextInputFormatter.deny(new RegExp(r"\s\b|\b\s"))
-      ],
-      decoration: InputDecoration(
-          suffixStyle: xLargeTextStyle(Colors.black, FontWeight.w400),
-          suffix: Text("AOA",
-              style: xLargeTextStyle(Colors.black, FontWeight.w400)),
-          prefixIcon: IconButton(
-              onPressed: action,
-              icon: Icon(Icons.info_outline, color: mTextColor)),
-          border: InputBorder.none,
-          hintText: "0.00"),
-      textAlign: TextAlign.center,
-      style: xLargeTextStyle(Colors.black, FontWeight.w400),
-      keyboardType: TextInputType.numberWithOptions(decimal: true),
-    ),
+InputDecoration textFieldDecoration(Widget prefixIcon) {
+  return InputDecoration(
+    hintText: "0.00",
+    suffixText: ' AKZ',
+    suffixStyle: xLargeTextStyle(Colors.black, FontWeight.w400),
+    prefixIcon: prefixIcon,
+    border: InputBorder.none,
   );
 }
--- mobileapp/lib/ui/mainScreen.dart
+++ mobileapp/lib/ui/mainScreen.dart
@@ -1,11 +1,15 @@
 import 'package:countup/countup.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mobileapp/businessLogic/IRTCalculator.dart';
 import 'package:mobileapp/constants/concepts.dart';
 import 'package:mobileapp/ui/InformationScreen.dart';
 import 'package:mobileapp/ui/common.dart';
+import 'package:intl/intl.dart';
 
 class MainScreen extends StatefulWidget {
   @override
@@ -14,189 +18,316 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   IRTCalculator calculator;
+  bool isLoading = false;
+  bool isEnabled = false;
+
+  final double minAllowedBaseSalary = 1.00;
 
   double baseSalary;
   double taxableBonus;
   double zeroCount = 0;
 
-  TextEditingController baseSalaryController = TextEditingController();
-  TextEditingController taxableBonusController = TextEditingController();
+  TextEditingController baseSalaryController =
+      MoneyMaskedTextController(decimalSeparator: '.', thousandSeparator: ',');
+  TextEditingController taxableBonusController =
+      MoneyMaskedTextController(decimalSeparator: '.', thousandSeparator: ',');
+
+  var formatter = NumberFormat('0,000');
 
   @override
   void initState() {
     super.initState();
     calculator = new IRTCalculator();
+
+    baseSalaryController.addListener(() {
+      setState(() {
+        isEnabled =
+            double.parse(baseSalaryController.text.replaceAll(",", "")) >=
+                minAllowedBaseSalary;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    baseSalaryController.dispose();
+    taxableBonusController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    var screen = MediaQuery.of(context).size;
+
     return SafeArea(
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: mtextCOlor2,
-        body: Column(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Flexible(
-              flex: 2,
-              child: Container(
-                color: Colors.white,
-                child: Column(
-                  children: [
-                    appbar(),
-                    //Salario Base
-                    customHeader("Salário Base"),
-                    customTextField(baseSalaryController, () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (_) => InformationScreen(
-                              "Salário Base", ccpBaseSalary)));
-                    }),
-                    //subsidio tributavel
-                    customHeader("Subsídio Tributável"),
-                    customTextField(taxableBonusController, () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (_) => InformationScreen(
-                              "Subsídio Tributável", ccpTaxableBonus)));
-                    }),
-                  ],
-                ),
-              ),
-            ),
+      child: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        onLongPress: () => FocusScope.of(context).unfocus(),
+        child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          backgroundColor: mSecondaryColor,
 
-            //result table
-            Expanded(
-                flex: 3,
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      resultListHeader("Tabela de Resultados",
-                          Icon(Icons.keyboard_arrow_down)),
-                      resultListItem(
-                          InformationScreen("Valor Bruto", ccpGrossIncome),
-                          "Valor Bruto",
-                          calculator.grossIncome,
-                          mTextColor),
+          //apbar
+          appBar: _appbar(),
 
-                      resultListItem(InformationScreen("INSS", ccpGrossIncome),
-                          "INSS", calculator.inss, mTextColor),
-
-                      resultListItem(
-                          InformationScreen("Valor Tributável", ccpGrossIncome),
-                          "Valor Tributável",
-                          calculator.taxableAmount,
-                          mTextColor),
-
-                      resultListItem(InformationScreen("IRT", ccpGrossIncome),
-                          "IRT", calculator.irt, mTextColor),
-
-                      resultListItem(
-                          InformationScreen("Valor Líquido", ccpGrossIncome),
-                          "Valor Líquido",
-                          calculator.netSalary,
-                          Colors.green),
-
-                      //additional information
-                      SizedBox(height: 15),
-                      resultListHeader("Informação Adicional",
-                          Icon(Icons.keyboard_arrow_down)),
-                      resultListItem(
-                          InformationScreen("Parcela Fixe", ccpGrossIncome),
-                          "Parcela Fixe",
-                          calculator.fixedParcel,
-                          mTextColor),
-
-                      resultListItem(
-                          InformationScreen("Excesso", ccpGrossIncome),
-                          "Excesso",
-                          calculator.excess,
-                          mTextColor),
-
-                      resultListItem(InformationScreen("Taxa", ccpGrossIncome),
-                          "Taxa", calculator.tax, mTextColor, "%"),
-                    ],
+          //body
+          body: Stack(
+            alignment: Alignment.topCenter,
+            children: [
+              //level1 ============================================
+              Column(
+                children: [
+                  //top part
+                  Card(
+                    margin: EdgeInsets.all(0),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(30),
+                            bottomRight: Radius.circular(30))),
+                    child: Container(
+                      height: screen.height * .32,
+                      child: Column(
+                        children: [
+                          //Salario Base
+                          SizedBox(
+                            height: 20,
+                          ),
+                          customHeader("Salário Base"),
+                          customTextField(baseSalaryController),
+                          //subsidio tributavel
+                          customHeader("Subsídio Tributável"),
+                          customTextField(taxableBonusController),
+                        ],
+                      ),
+                    ),
                   ),
-                )),
 
-            //button
-            InkWell(
-              onTap: () {
-                baseSalary = _prepareDouble(baseSalaryController.text);
-                taxableBonus = _prepareDouble(taxableBonusController.text);
-                setState(() {
-                  calculator.calculate(baseSalary, taxableBonus);
-                });
-              },
-              child: customFullButton(context, 'Calcular IRT'),
-            )
-          ],
+                  //result table
+                  Expanded(
+                    child: ListView(
+                      children: [
+                        SizedBox(height: 20),
+
+                        //First section header
+                        ListTile(
+                            minLeadingWidth: 0,
+                            title: Text("Tabela de Resultados",
+                                style: smallTextStyle(
+                                    mTextColor, FontWeight.bold))),
+                        resultListItem(
+                            InformationScreen("Valor Bruto", ccpGrossIncome),
+                            "Valor Bruto",
+                            calculator.grossIncome,
+                            mTextColor),
+
+                        resultListItem(
+                            InformationScreen("INSS", ccpGrossIncome),
+                            "INSS",
+                            calculator.inss,
+                            mTextColor),
+
+                        resultListItem(InformationScreen("IRT", ccpGrossIncome),
+                            "IRT", calculator.irt, mTextColor),
+
+                        resultListItem(
+                            InformationScreen("Valor Líquido", ccpGrossIncome),
+                            "Valor Líquido",
+                            calculator.netSalary,
+                            mPrimaryColor,
+                            " AKZ",
+                            20.0),
+
+                        //additional information
+                        SizedBox(height: 15),
+
+                        ExpansionTile(
+                          title: Text(
+                            "Informação Adicional",
+                            style: smallTextStyle(mTextColor, FontWeight.bold),
+                          ),
+                          children: [
+                            resultListItem(
+                                InformationScreen(
+                                    "Valor Tributável", ccpGrossIncome),
+                                "Valor Tributável",
+                                calculator.taxableAmount,
+                                mTextColor),
+                            resultListItem(
+                                InformationScreen(
+                                    "Parcela Fixa", ccpGrossIncome),
+                                "Parcela Fixe",
+                                calculator.fixedParcel,
+                                mTextColor),
+                            resultListItem(
+                                InformationScreen("Excesso", ccpGrossIncome),
+                                "Excesso",
+                                calculator.excess,
+                                mTextColor),
+                            resultListItem(
+                                InformationScreen("Taxa", ccpGrossIncome),
+                                "Taxa",
+                                calculator.tax,
+                                mTextColor,
+                                " %"),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+
+              //leve2 ============================================
+              //button to calculate
+              Align(
+                alignment: Alignment(0, -.3),
+                child: ElevatedButton(
+                  autofocus: false,
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
+                    backgroundColor:
+                        MaterialStateProperty.resolveWith((states) {
+                      if (states.contains(MaterialState.disabled))
+                        return Colors.grey[300];
+                      return mPrimaryColor;
+                    }),
+                    foregroundColor:
+                        MaterialStateProperty.resolveWith((states) {
+                      if (states.contains(MaterialState.disabled))
+                        return Colors.grey[600];
+                      return Colors.white;
+                    }),
+                    padding: MaterialStateProperty.all(EdgeInsets.symmetric(
+                        horizontal: screen.width * 0.2, vertical: 10)),
+                  ),
+                  onPressed: isEnabled ? () => calculate() : null,
+                  child: Text(
+                    "Calcular IRT",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 
-  //if input is not right format convert it back to 0
-  double _prepareDouble(input) {
-    return (input.isEmpty) ? 0 : double.parse(input);
+  void copyToClipBoard(String title, String value) {
+    Clipboard.setData(new ClipboardData(text: value));
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        backgroundColor: mPrimaryColor,
+        duration: Duration(milliseconds: 3000),
+        content: Text('O valor de $title Foi copiado para a prancheta'),
+      ),
+    );
   }
 
-  Widget resultListHeader(String title, Widget trailing) {
+  void calculate() {
+    baseSalary = _prepareDouble(baseSalaryController.text);
+    taxableBonus = _prepareDouble(taxableBonusController.text);
+    setState(() {
+      isLoading = true;
+      calculator.calculate(baseSalary, taxableBonus);
+    });
+  }
+
+  //Make Sure inoput is in correct format and appropriate number
+  double _prepareDouble(String input) {
+    if (input.isEmpty) {
+      return 0.00;
+    } else {
+      return double.parse(input.replaceAll(",", ""));
+    }
+  }
+
+  //specific textfield data for this screen top zone (input)
+  Widget customTextField(TextEditingController controller) {
     return Container(
-      margin: EdgeInsets.only(right: 5),
-      child: ListTile(
-          minLeadingWidth: 0,
-          title: Text(
-            title,
-            style: smallTextStyle(mTextColor, FontWeight.bold),
-          ),
-          trailing: trailing),
+      margin: EdgeInsets.symmetric(horizontal: 30),
+      child: TextField(
+        controller: controller,
+        maxLines: 1,
+        textInputAction: TextInputAction.done,
+        decoration: textFieldDecoration(IconButton(
+          onPressed: () {
+            controller.text = 0.00.toString();
+            calculate();
+          },
+          icon: Icon(Icons.clear),
+          color: mTextColor2,
+        )),
+        textAlign: TextAlign.center,
+        style: GoogleFonts.sairaCondensed(
+            fontSize: 30.0, color: Colors.black, fontWeight: FontWeight.w500),
+        keyboardType: TextInputType.numberWithOptions(decimal: true),
+      ),
     );
   }
 
   Widget resultListItem(Widget screen, String title, double value, Color color,
-      [String suffix = "AOA"]) {
+      [String suffix = " AKZ", double size = 14]) {
     return InkWell(
       onTap: () {
         Navigator.push(context, MaterialPageRoute(builder: (_) => screen));
       },
+      onLongPress: () => copyToClipBoard(title, value.toString()),
       child: Container(
         margin: EdgeInsets.only(right: 5),
         child: ListTile(
           minLeadingWidth: 0,
-          leading: Icon(Icons.info_outline, color: mTextColor),
+          //leading: Icon(Icons.info_outline, color: mTextColor),
           title:
               Text(title, style: TextStyle(color: Colors.grey, fontSize: 14)),
           trailing: Countup(
             begin: zeroCount,
             end: value,
             suffix: suffix,
+            precision: 2,
             duration: Duration(seconds: 1),
             separator: ',',
             style: TextStyle(
-                color: color, fontSize: 14, fontWeight: FontWeight.bold),
+                color: color, fontSize: size, fontWeight: FontWeight.bold),
           ),
         ),
       ),
     );
   }
 
-  Widget appbar() {
-    return ListTile(
-      title:
-          Text("Angola", style: TextStyle(color: Colors.black, fontSize: 14)),
-      subtitle: Text("Calculadora IRT",
-          style: TextStyle(color: Colors.black, fontSize: 14)),
-      trailing: IconButton(
-        onPressed: () {},
-        icon: Icon(FontAwesomeIcons.thLarge, color: Colors.blueGrey, size: 16),
+  Widget _appbar() {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: Colors.white,
+      title: ListTile(
+        title:
+            Text("Angola", style: TextStyle(color: Colors.black, fontSize: 14)),
+        subtitle: Text("Calculadora IRT",
+            style: TextStyle(color: Colors.black, fontSize: 14)),
       ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.info_outlined, color: mTextColor2, size: 22),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(FontAwesomeIcons.calculator, color: mTextColor2, size: 16),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(FontAwesomeIcons.stream, color: mTextColor2, size: 16),
+        )
+      ],
     );
   }
 
   Widget customHeader(String header) {
     return Container(
-        margin: EdgeInsets.only(top: 15),
+        margin: EdgeInsets.only(top: 10),
         child:
-            Text(header, style: smallTextStyle(Colors.grey, FontWeight.w300)));
+            Text(header, style: smallTextStyle(mTextColor2, FontWeight.w300)));
   }
 }
